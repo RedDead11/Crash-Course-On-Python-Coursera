d82287c82b5edcdac0a8cbb13567360300a5a120
Daud <85739147+RedDead11@users.noreply.github.com>
2023-08-08 21:40:31 +0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+file_counts = {"jpg":10, "txt":14, "csv":2 ,"py":23}
+
+#print(file_counts)
+
+
+"jpg" in file_counts
+
+# Returns True or False
+
+
+file_counts["cfg"] = 8
+
+print(file_counts)
+
+del file_counts["cfg"]
+
+print(file_counts)
